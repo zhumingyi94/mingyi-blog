dab204bba91b478e09133ccddbc5fe2efe268c09
nhatdm22022537 <dominhnhat2004@gmail.com>
2024-08-03 10:34:59 +0700

    Quartz sync: Aug 3, 2024, 10:34 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,7 +27,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
-    Component.RecentNotes({ limit: 5 }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.DesktopOnly(Component.Explorer()),
   ],
@@ -36,6 +35,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Graph(),
     Component.TagList(),
     Component.Backlinks(),
+    Component.RecentNotes({ limit: 2 }),
   ],
 }
 
