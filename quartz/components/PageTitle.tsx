43917ad1213099018dd40f1e632b5ac1a244b21a
nhatdm22022537 <dominhnhat2004@gmail.com>
2024-08-05 04:05:50 +0700

    Quartz sync: Aug 5, 2024, 4:05 AM

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -1,16 +1,18 @@
 import { pathToRoot } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
+import { i18n } from "../i18n"
 
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
-  const imageUrl = cfg?.pageTitleImage ?? "icon.png"  // Replace with your default image path
+  const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
   return (
-    <h1 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>
-        <img src={imageUrl} alt="Page Title" class="page-title-image" />
-      </a>
-    </h1>
+    <div>
+      <div class="rectangle"></div>
+      <h1 class={classNames(displayClass, "page-title")}>
+        <a href={baseDir}>{title}</a>
+      </h1>
+    </div>
   )
 }
 
@@ -18,9 +20,12 @@ PageTitle.css = `
 .page-title {
   margin: 0;
 }
-.page-title-image {
-  max-width: 100%;
-  height: auto;
+
+.rectangle {
+  width: 100px; /* Adjust width as needed */
+  height: 50px; /* Adjust height as needed */
+  background-color: #05FF00;
+  margin-bottom: 10px; /* Space between rectangle and title */
 }
 `
 
