de781b8bd5aff6002f460cbb108f7a8b2779986f
nhatdm22022537 <dominhnhat2004@gmail.com>
2024-08-05 03:18:30 +0700

    Quartz sync: Aug 5, 2024, 3:18 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "không một.",
+    pageTitle: "🟢 không một.",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
